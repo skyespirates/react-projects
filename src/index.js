a9ae786cb70f8e2fcd342bd3e34d68e577a2a28d
Skyes Crawford <akashiseijurou414@gmail.com>
2021-03-09 06:51:52 +0700

    add first project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./birthday-reminder/index.css";
+import App from "./birthday-reminder/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
