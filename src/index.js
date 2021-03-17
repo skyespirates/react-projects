f75b86aa3ac0b5a4638ccfd72bbe5470df2f384b
Skyes Crawford <akashiseijurou414@gmail.com>
2021-03-17 08:09:10 +0700

    restructuring with react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./reviews/index.css";
-import App from "./reviews/App";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Birthday from "./birthday-reminder/App";
+import Reviews from "./reviews/App";
+import Tours from "./tours/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <Route path="/birthday">
+        <Birthday />
+      </Route>
+      <Route path="/reviews">
+        <Reviews />
+      </Route>
+      <Route path="/tours">
+        <Tours />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+function Navbar() {
+  return (
+    <nav>
+      <ul style={{ display: "flex" }}>
+        <li>
+          <Link to="/birthday">Birthday</Link>
+        </li>
+        <li>
+          <Link to="/reviews">Reviews</Link>
+        </li>
+        <li>
+          <Link to="/tours">Tours</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
